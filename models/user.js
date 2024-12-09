b7071a638c06d92493068d596377f301dbc85788
AZAT MANAS <ZTMANAS@GMAIL.COM>
2024-12-08 21:32:56 -0500

    user.js email,password updated

--- models/user.js
+++ models/user.js
@@ -3,6 +3,16 @@ const validator = require("validator");
 
 const userSchema = new moongose.Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+    },
+  },
   avatar: {
     type: String,
     required: true,
@@ -12,6 +22,11 @@ const userSchema = new moongose.Schema({
       },
       message: "You must enter a valid URL",
     },
+  },
+  password: {
+    required: true,
+    type: String,
+    select: false,
   },
 });
 module.exports = moongose.model("user", userSchema);

2384fc6880ec1aae056573646139c26b94a1af6b
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-03-23 12:22:49 -0400

    validation js updated

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -6,7 +6,7 @@ const validations = () => {
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().required().min(2).max(30),
-    age: Joi.number().integer().required().min(18),
+    URL: Joi.number().integer().required().min(18),
     about: Joi.string().min(2).max(30),
   });
 };

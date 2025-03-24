d7c6b802b0d3ea40fdf8cad612720490f67ca294
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-03-24 12:52:55 -0400

    logger js created

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const cors = require("cors");
 const mainRouter = require("./routes/index");
 const validator = require("validator");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
+app.use(requestLogger);
 app.use(cors());
 app.use(express.json());
+app.use(errorLogger);
 app.use("/", mainRouter);
 app.use("/", validator);
 app.use(errors());

b58a98eb079eeb390d42e6a3d736811e89762aa8
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-03-30 20:19:19 -0400

    config js updated

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,8 @@
-const JWT_SECRET = "your-secret-key";
+const { JWT_SECRET = "super-strong-secret" } = process.env;
 
-module.exports = { JWT_SECRET };
+module.exports = {
+  JWT_SECRET,
+};
+// const JWT_SECRET = "your-secret-key";
+
+// module.exports = { JWT_SECRET };

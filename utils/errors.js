2c84f1ea88f266b821c2f6d7896dccf681b06cf5
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-04-15 13:07:59 -0400

    additional error status created

--- utils/errors.js
+++ utils/errors.js
@@ -17,11 +17,29 @@ module.exports = {
   CONFLICT,
   DEFAULT,
 };
-// class NotFoundError extends Error {
-//   constructor(message) {
-//     super(message);
-//     this.statusCode = 404;
-//   }
-// }
+class NotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+  }
+}
 
-// module.exports = NotFoundError;
+module.exports = NotFoundError;
+
+class BadRequest extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+module.exports = BadRequest;
+
+class UnAuthorized extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 400;
+  }
+}
+
+module.exports = UnAuthorized;

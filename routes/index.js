9942a7691bdd8afe9f0379e06c5500f070dc5968
AZAT MANAS <ZTMANAS@GMAIL.COM>
2025-05-10 15:47:57 -0400

    routes js updated

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,18 @@ const clothingItemRouter = require("./clothingItems");
 const { login, createUser } = require("../controllers/users");
 const NotFoundError = require("../utils/notFoundError");
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 mainRouter.post("/signin", login);
 mainRouter.post("/signup", createUser);
 mainRouter.use("/users", usersRouter);
 mainRouter.use("/items", clothingItemRouter);
-mainRouter.use((res) => {
-  res.status(NotFoundError).send({ message: "Requested resource not found" });
+router.use((req, res, next) => {
+  next(new NotFoundError("Page is not found"));
 });
 
 module.exports = mainRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { updateUsers, getCurrentUser } = require("../controllers/users");
 const authMiddleware = require("../middlewares/auth");
-
+const { validateCardBody } = require("../middlewares/validation");
 router.use(authMiddleware);
 router.get("/me", getCurrentUser);
-router.patch("/me", updateUsers);
+router.patch("/me", updateUsers, validateCardBody);
 
 module.exports = router;
